2e794c58042e01a43b66b1569a3023064c3e2f9f
GabMGallegos <gabriel25molina@hotmail.com>
2026-06-17 13:19:03 -0500

    m2

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.get('/health', (req, res) => {
     });
 });
 
-app.use('/computerstore', dataCustomerRoutes);
+app.use('/store', dataCustomerRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Data endpoint not found' });
